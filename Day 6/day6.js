2e9ca159297959558f1f2f639a5090b64cab570e
Kimani-Dominic <muiruridominic001@gmail.com>
2023-12-22 11:40:58 +0300

    Day 6

---
+++ Day 6/day6.js
@@ -0,0 +1,31 @@
+//Day 6 -- Objects and properties
+
+// Create an object that represents a book. Include properties like `title`, `author`, `year`, and `isAvailable`. 
+// Then, use both dot notation and bracket notation to access and display the book's information.
+
+
+
+let book = {
+    title: "Javascript 101",
+    author: "Kimani",
+    year: 2023,
+    isAvailable: true
+};
+
+ //using the dot notation
+console.log(book.isAvailable);
+    true
+console.log(book.author);
+    kimani
+         
+    
+
+// using the bracket notation  
+console.log(book);
+    {
+      title: 'Javascript 101',
+      author: 'Kimani',
+      year: 2023,
+      isAvailable: true
+    }
+    
